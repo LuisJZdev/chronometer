64ce84c5a6258d1987efbfc592d3aa6f7a68a991
beamerboy <luisjimenez10005@gmail.com>
2023-07-24 12:00:11 -0400

    adding events to selected times with

--- logic.js
+++ logic.js
@@ -1,4 +1,29 @@
-const hour = document.getElementsByName("hour");
-console.log(hour)
-console.log(hour[0])
-console.log(hour[0][0].getAttribute("value"))
+const times = {
+    hour: document.getElementById("hour"),
+    minute: document.getElementById("minute"),
+    second: document.getElementById("second")
+};
+
+const selectedTimes = {
+    hour: document.getElementById("hour-select"),
+    minute: document.getElementById("minute-select"),
+    second: document.getElementById("second-select")
+}
+
+console.log(times.hour);
+console.log(times.minute);
+console.log(times.second.textContent);
+
+console.log(selectedTimes.hour);
+console.log(selectedTimes.minute);
+console.log(selectedTimes.second);
+
+selectedTimes.hour.addEventListener("click", () => {
+    times.hour.innerText = selectedTimes.hour.value.length == 1? '0'+selectedTimes.hour.value: selectedTimes.hour.value;
+})
+selectedTimes.minute.addEventListener("click", () => {
+    times.minute.innerText = selectedTimes.minute.value.length == 1? '0'+selectedTimes.minute.value: selectedTimes.minute.value;
+})
+selectedTimes.second.addEventListener("click", () => {
+    times.second.innerText = selectedTimes.second.value.length == 1? '0'+selectedTimes.second.value: selectedTimes.second.value;
+})
